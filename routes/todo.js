ce1442cf19434d6ad6c0392a6000418babe16da7
buyaramadhan <ytchochote@gmail.com>
2024-10-29 10:36:12 +0700

    CRUD_RESTAPI

--- routes/todo.js
+++ routes/todo.js
@@ -12,4 +12,25 @@ let todos = [
 
 router.get('/', (req, res) =>{res.json(todos); });
 
+router.post('/', (req, res) => {
+    const newTodo = {
+        id: todos.length + 1,
+        task: req.body.task,
+        completed: false
+    };
+    todos.push(newTodo);
+    res.status(201).json(newTodo);
+});
+
 module.exports = router;
+
+router.put('/:id', (req, res) =>{
+    const todo = todos.find(t => t.id === parseInt(req.params.id));
+    if (!todo) return res.status(404).json({message:'Tugas tidak ditemukan'});
+    todo.task = req.body.task || todo.task;
+
+    res.status(200).json({
+        message: `Tugas dengan ID ${todo.id} telah diperbarui`,
+        updateTodo: todo
+    });
+});
